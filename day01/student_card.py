1003dda2289742e06b7638470456b459f4c84e98
Madhura Sarkar <157148416+Madhura-Sarkar@users.noreply.github.com>
2026-08-06 22:39:38 +0530

    creating student_card.py on day01

---
+++ day01/student_card.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+subject = input("Enter your subject: ")
+
+print("\n===== Student Card -----")
+print("Name: ", name)
+print("Age: ", age)
+print("Subject: ", subject)
+print("Welcome to AI Engineering!")
