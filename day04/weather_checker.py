8344fc17f1b82f1a8041ac5c6e5b4bac3c18c519
Madhura Sarkar <157148416+Madhura-Sarkar@users.noreply.github.com>
2026-08-09 21:58:12 +0530

    Add files via upload

---
+++ day04/weather_checker.py
@@ -0,0 +1,7 @@
+input = input("What is the weather? --> ")
+if input == "sunny":
+    print("Wear sunglasses.")
+elif input == "rainy":
+    print("rainy")
+else:
+    print("I don't have advice for that weather.")
