5a3fd5c1084d153a1fc2dca2b2bae9eb87f9faa1
Madhura Sarkar <157148416+Madhura-Sarkar@users.noreply.github.com>
2026-08-12 20:25:28 +0530

    update function.py with code

--- day07/function.py
+++ day07/function.py
@@ -1,1 +1,15 @@
+def greet():
+    print("Hello!")
+    print("Welcome!")
 
+greet()
+
+def introduce(name, age):
+    print("Name:", name)
+    print("Age:", age)
+
+introduce("Alice", 30)
+
+def add(a, b):
+    return a + b
+print(add(5, 10))
