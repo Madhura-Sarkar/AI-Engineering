c031ef60aac6cf114cd4d5c37c0cf0dd5e686e7e
Madhura Sarkar <157148416+Madhura-Sarkar@users.noreply.github.com>
2026-08-12 20:26:11 +0530

    upload day07 practice materials

---
+++ day07/greet.py
@@ -0,0 +1,5 @@
+def greet(name):
+    print("Hello", name)
+    print("Have a great day!")
+
+greet("Madhura")
---
+++ day07/practice.py
@@ -0,0 +1,15 @@
+def multiply(a, b):
+    return a * b
+print(multiply(3, 4))
+
+def largest(a, b):
+    if a > b:
+        print(f"{a} is largest")
+    else:
+        print(f"{b} is largest")
+
+largest(10, 5)
+
+def greet(name):
+    print("Welcome", name)
+greet("Madhura")
