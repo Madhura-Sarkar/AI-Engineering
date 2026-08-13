443a012550fb11e8901de7f2cd5d519544384d6f
Madhura Sarkar <157148416+Madhura-Sarkar@users.noreply.github.com>
2026-08-14 01:23:50 +0530

    update dictionary.py

--- day09/dictionary.py
+++ day09/dictionary.py
@@ -1,1 +1,27 @@
+#1
+book = {
+    "title": "Atomic Habits",
+    "author": "James Clear",
+    "pages": 320
+}
+
+print(book["title"])
+print(book["author"])
+
+#2
+car = {
+    "brand": "Toyota",
+    "year": 2022
+}
+car["color"] = "Black"
+print(car)
+
+#3
+person = {
+    "name": "Alex",
+    "age": 25,
+    "city": "London"
+}
+for key in person:
+    print(key, ":", person[key])
 
