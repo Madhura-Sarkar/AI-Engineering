495fb8c4e2c6a1a2d0eb1e2f089aab9538e0b9fc
Madhura Sarkar <157148416+Madhura-Sarkar@users.noreply.github.com>
2026-08-13 20:01:27 +0530

    update lists.py with code

--- day08/lists.py
+++ day08/lists.py
@@ -1,1 +1,12 @@
+colors = ["Red", "Blue", "Green"]
+print(colors[0])
+print(colors[2])
 
+numbers = [10, 20, 30]
+numbers.append(40)
+print(numbers)
+
+# loops in lists
+animals = ["Cat", "Dog", "Rabbit"]
+for animal in animals:
+    print(animal)
