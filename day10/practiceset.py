95941eb6d3ba4170964a1ddd77e2d0a6f7e197fc
Madhura Sarkar <157148416+Madhura-Sarkar@users.noreply.github.com>
2026-08-16 02:59:24 +0530

    Add files via upload

---
+++ day10/practiceset.py
@@ -0,0 +1,18 @@
+fruits = {"Apple", "Banana", "Mango"}
+fruits.add("Orange")
+print(fruits)
+
+A = {10,20,30,40}
+B = {30,40,50,60}
+print(A|B)
+print(A&B)
+print(A-B)
+print(A^B)
+
+num = [5,5,10,10,15,20,20]
+unique_num = set(num)
+print(unique_num)
+
+languages = {"Python", "HTML", "CSS", "JavaScript"}
+print("CSS" in languages)
+print("Java" in languages)
