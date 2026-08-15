5344410af93cae9691fe907b9d97a0de848f3a26
Madhura Sarkar <157148416+Madhura-Sarkar@users.noreply.github.com>
2026-08-16 02:59:45 +0530

    Add files via upload

---
+++ day10/practice.py
@@ -0,0 +1,15 @@
+languages = ("Python", "Java", "C++", "JavaScript", "Ruby")
+print(languages[0])
+print(languages[4])
+
+numbers = (1,2,3,4,5,6)
+print(numbers[1:4])
+
+num = (10,20,30,20,40,40,50,60)
+print(num.count(40))
+
+student="Madhura",22,"Adobe Creative Suite"
+name,age,favourite_software = student
+print(name)
+print(age)
+print(favourite_software)
---
+++ day10/tuples.py
@@ -0,0 +1,26 @@
+fruits= ("Apple","Banana","Mango")
+print(fruits)
+
+numbers= (5,10,15,20)
+print(numbers[0])
+print(numbers[-2])
+
+numbers= (10,20,30,40,50)
+print(numbers[1:4])
+
+colors= ("Red","Blue","Green")
+print(len(colors))
+
+# tuple packing
+student="Madhura",22,"Python"
+print(student)
+
+# tuple unpacking 
+name,age,skill=student
+print(name)
+print(age)
+print(skill)
+
+# tuple count 
+numbers= (1,2,3,2,2,5)
+print(numbers.count(2))
