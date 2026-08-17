5220952a81f5ae1cb7a5ba0cc3e482bde70934ad
Madhura Sarkar <157148416+Madhura-Sarkar@users.noreply.github.com>
2026-08-17 19:51:12 +0530

    upload aboutme.py

--- day11/aboutme.py
+++ day11/aboutme.py
@@ -1,1 +1,16 @@
+with open("about_me.txt", "w") as file:
+    file.write("Name: Madhura\n")
+    file.write("Course: AI Engineering\n")
+    file.write("Day: 11\n")
+    file.write("Goal: Become an AI Engineer\n")
+    
+with open("about_me.txt", "r") as file:
+    content = file.read()
+print(content)
 
+with open("about_me.txt", "a") as file:
+    file.write("Current Skill: Python\n")
+    
+with open("about_me.txt", "r") as file:
+    content = file.read()
+print(content)
